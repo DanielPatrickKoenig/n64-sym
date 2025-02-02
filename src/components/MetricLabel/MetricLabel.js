74f5ec1e2e45dbf7a7699388b82b361d0cebf579
dan koenig <dangerbird@dans-MacBook-Pro.local>
2025-02-02 14:02:45 -0500

    make labels fade in

--- src/components/MetricLabel/MetricLabel.js
+++ src/components/MetricLabel/MetricLabel.js
@@ -1,8 +1,13 @@
+import { useEffect, useState } from 'react';
 import './MetricLabel.css';
 const MetricLabel = (props) => {
+    const [toggleValue, setToggleValue] = useState(1);
+    useEffect(() => {
+        setToggleValue(toggleValue === 1 ? 2 : 1);
+    }, [props.patern]);
     return (
         <div
-            className={`metric-label ${props.patern} ${props.side}`}
+            className={`metric-label metric-label-${toggleValue} ${props.patern} ${props.side}`}
             style={{left: `${props.x}%`, top: `${props.y}%`}}
         >
             <label>{props.label}</label>
