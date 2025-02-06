1d287fc45c32b22449d3f4b53d8cee42d0c9a192
dan koenig <dangerbird@dans-MacBook-Pro.local>
2025-02-06 18:48:23 -0500

    add icons

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react"
 import './AppHeader.css';
+import FilterIcon from "../FilterIcon/FilterIcon";
+import MetricsIcon from "../MetricsIcon/MetricsIcon";
 
 const AppHeader = (props) => {
     const [showFilters, setShowFilters] = useState(false);
     const [showSorters, setShowSorters] = useState(false);
+    const filterCount = () => {
+        return Object.keys(props.filters).map(item => props.filters[item].length).reduce((t, item) => item + t, 0);
+    }
     useEffect(() => {
         console.log('CHANGE');
         setShowSorters(false);
@@ -12,8 +17,19 @@ const AppHeader = (props) => {
     return (
         <header className={`app-header ${showFilters ? 'show-filters' : ''} ${showSorters ? 'show-sorters' : ''}`}>
             <h1>Particle Chart</h1>
-            <button onClick={() => { setShowSorters(!showSorters); setShowFilters(false); }}>Sorters</button>
-            <button onClick={() => { setShowFilters(!showFilters); setShowSorters(false); }}>Filters</button>
+            <button onClick={() => { setShowSorters(!showSorters); setShowFilters(false); }}>
+                <MetricsIcon />
+                <span>METRICS</span>
+            </button>
+            <button onClick={() => { setShowFilters(!showFilters); setShowSorters(false); }}>
+                <FilterIcon />
+                <span>FILTERS</span>
+                {filterCount() > 0 && <span
+                    className="filter-count"
+                >
+                    {filterCount()}
+                </span>}
+            </button>
         </header>
     )
 }
